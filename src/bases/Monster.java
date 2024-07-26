ca82a5fa2a00e48e821ab367c764c58fb07ae2fb
NaoyaYamamoto <175591871+tarochan100@users.noreply.github.com>
2024-07-26 14:39:04 +0900

    更新

--- src/bases/Monster.java
+++ src/bases/Monster.java
@@ -1,5 +1,21 @@
 package bases;
 
-public class Monster {
+// モンスターを表現する抽象クラス
+public abstract class Monster extends Living {
 
+	// コンストラクタ
+	public Monster(String name, String weapon) {
+		// Livingクラスで定義したコンストラクタを利用する
+	}
+
+	// attackメソッドのオーバーライド
+	@Override
+	public void attack(Living target) {
+		// 1から10までのサイコロを振り、自分の攻撃力とかけ合わせた値を相手に与えるダメージとする
+		
+		// 相手のHPをダメージ値だけ減らす
+		
+		// コンソールにステータスを表示
+		
+	}
 }
